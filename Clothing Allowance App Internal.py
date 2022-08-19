34070b5b31a94b59b44ac9427c3766ec815f5934
Toby Roberts <tobyhazeroberts@gmail.com>
2022-08-19 15:44:32 -0600

    Simple Child Class

--- Clothing Allowance App Internal.py
+++ Clothing Allowance App Internal.py
@@ -1,2 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+
+class Child:
+	'''Collects all info about each child, 
+	stores it and then displays it.'''
+
+	# Collects and stores all childs info.
+
+	def __init__(self, name, allowance, bonus_cost):
+		self.name = name
+		self.allowance = allowance
+		self.bonus_cost = bonus_cost
+
+	
+	def get_details(self):
+		print("Name: {}" .format(self.name))
+		print("Allowance: ${}" .format(self.allowance))
+		print("Bonus cost: ${}" .format(self.bonus_cost))
+		print("-----------------")
